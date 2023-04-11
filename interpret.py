3f018bce87cee6f9f4fba1fe41dc7c0b167f89cd
rr3333mmAA <nice.amirkhan@gmail.com>
2023-04-11 19:50:26 +0200

    Argument parser added.

--- interpret.py
+++ interpret.py
@@ -7,4 +7,20 @@ Allowed libs
     xml.etree.ElementTree
     xml.parsers.expat
     xml.sax.handler
+
+Tests cheat sheet:
+src - source/input file
+rc - return code
+out - output
 """
+import argparse
+
+# Argument parser (--help, --source=file, --input=file)
+parser = argparse.ArgumentParser(
+    description='Script loads an XML representation of a program '
+                'and interprets it using input based on command-line parameters, generating output.')
+parser.add_argument('--source', help="Source XML file")  # Change help
+parser.add_argument('--input', help="Input")  # Change help
+args = parser.parse_args()
+
+
