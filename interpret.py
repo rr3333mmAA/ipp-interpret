106eea2210fb5fce5c4b17d05f21d1d8a947f155
rr3333mmAA <nice.amirkhan@gmail.com>
2023-04-13 23:32:13 +0200

    Argument parser added.

--- interpret.py
+++ interpret.py
@@ -1,19 +1,13 @@
 """
-Allowed libs
-    argparse OR getopt
-    codecs
-    re
-    xml.dom.minidom
-    xml.etree.ElementTree
-    xml.parsers.expat
-    xml.sax.handler
-
 Tests cheat sheet:
 src - source/input file
 rc - return code
 out - output
 """
 import argparse
+import xml.etree.ElementTree as ET
+import codecs  # encoder, decoder
+import re  # regEx
 
 # Argument parser (--help, --source=file, --input=file)
 parser = argparse.ArgumentParser(
@@ -24,3 +18,30 @@ parser.add_argument('--input', help="Input")  # Change help
 args = parser.parse_args()
 
 
+# TODO:
+#  check order and arg number via
+#  @property and @method.setter
+#  or
+#  create check decorator
+
+# XML Element Tree
+if args.source or args.input:
+
+    tree = ET.parse(args.source)
+    root = tree.getroot()
+
+    '''
+    Format: instructions = [(instruction, [(type, text)]), ...]
+    Example:
+    instructions = [
+    ('DEFVAR', [('var', 'GF@a')]),
+    ('READ', [('var': 'GF@a'), ('type': 'int)])
+    ]
+    '''
+    instructions = (
+        (
+            instruction.get('opcode'),
+            [(arg.get('type'), arg.text.strip()) for arg in instruction]
+        )
+        for instruction in root.findall('instruction')
+    )
