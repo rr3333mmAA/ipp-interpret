dfe6a450e81e94a567c93a1f311e5ad93e1a75a0
rr3333mmAA <nice.amirkhan@gmail.com>
2023-04-18 13:29:30 +0200

    FLOAT, STACK

--- interpret.py
+++ interpret.py
@@ -19,6 +19,8 @@ TODO:
  wrong args
  arg order
  32 and 52 error
+ type() change to isinstance()
+ docs: add concats
 """
 import argparse
 import xml.etree.ElementTree as ET
@@ -48,15 +50,25 @@ class Argument:
             try:
                 int(text)
             except ValueError:
-                exit(32) # Error
+                exit(32)  # Error
         elif arg_type == 'string':
             matches = set(re.findall(r'\\[0-9]{3}', text))
             for match in matches:
                 text = text.replace(match, chr(int(match[1:])))
         elif arg_type == 'type':
             text = text.lower()
-            if text not in {'int', 'string', 'bool'}:
+            if text not in {'int', 'string', 'bool', 'float'}:
                 exit(56)  # Error
+        elif arg_type == 'float':
+            try:
+                text = float.fromhex(text)
+            except ValueError:
+                pass  # Error
+
+            try:
+                float.hex(float(text))
+            except ValueError:
+                exit(32)  # Error
         return [arg_type, text]
 
 
@@ -73,6 +85,7 @@ class Instruction:
 
         Instruction.all_.append(self)
 
+    # Redesign
     @classmethod
     def get_instructions_from_xml(cls, src: str) -> list:
         """
@@ -177,41 +190,111 @@ class Function:
         self._set_value(var, value)
         self.stack.pop()
 
+    def instr_clears(self) -> None:
+        self.stack = []
+
     def instr_add(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, '+')
         self._set_value(var, value)
+
+    def instr_adds(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) != int:
+            exit(53)  # Error
+        value = value1 + value2
+        self._replace_stack_value(value, 2)
 
     def instr_sub(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, '-')
         self._set_value(var, value)
 
+    def instr_subs(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) != int:
+            exit(53)  # Error
+        value = value2 - value1
+        self._replace_stack_value(value, 2)
+
     def instr_mul(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, '*')
         self._set_value(var, value)
 
-    def instr_idiv(self, var: str, symb1: str, symb2: str) -> None:
+    def instr_muls(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) != int:
+            exit(53)  # Error
+        value = value1 * value2
+        self._replace_stack_value(value, 2)
+
+    def instr_div(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, '/')
         self._set_value(var, value)
+
+    def instr_idiv(self, var: str, symb1: str, symb2: str) -> None:
+        value = self._operator(symb1, symb2, '//')
+        self._set_value(var, value)
+
+    def instr_idivs(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) != int:
+            exit(53)  # Error
+        value = value2 // value1
+        self._replace_stack_value(value, 2)
 
     def instr_lt(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, '<')
         self._set_value(var, value)
 
+    def instr_lts(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) or value1 is None:
+            exit(53)  # Error
+        value = value2 < value1
+        self._replace_stack_value(value, 2)
+
     def instr_gt(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, '>')
         self._set_value(var, value)
+
+    def instr_gts(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) or value1 is None:
+            exit(53)  # Error
+        value = value2 > value1
+        self._replace_stack_value(value, 2)
 
     def instr_eq(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, '==')
         self._set_value(var, value)
 
+    def instr_eqs(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) and value1 is not None and value2 is not None:
+            exit(53)  # Error
+        value = value1 == value2
+        self._replace_stack_value(value, 2)
+
     def instr_and(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, 'and')
         self._set_value(var, value)
 
+    def instr_ands(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if not isinstance(value1, bool) or not isinstance(value2, bool):
+            exit(53)  # Error
+        value = value1 and value2
+        self._replace_stack_value(value, 2)
+
     def instr_or(self, var: str, symb1: str, symb2: str) -> None:
         value = self._operator(symb1, symb2, 'or')
         self._set_value(var, value)
+
+    def instr_ors(self) -> None:
+        value1, value2 = self._get_values_from_stack(2)
+        if not isinstance(value1, bool) or not isinstance(value2, bool):
+            exit(53)  # Error
+        value = value1 or value2
+        self._replace_stack_value(value, 2)
 
     # Redesign
     def instr_not(self, var: str, symb1: str) -> None:
@@ -219,6 +302,13 @@ class Function:
         if not isinstance(value, bool):
             exit(53)  # Error
         self._set_value(var, not bool(value))
+
+    def instr_nots(self) -> None:
+        value = self._get_values_from_stack(1)
+        if not isinstance(value, bool):
+            exit(53)  # Error
+        value = not(value)
+        self._replace_stack_value(value, 1)
 
     def instr_int2char(self, var: str, symb: str) -> None:
         value = self._get_value(symb)
@@ -229,6 +319,35 @@ class Function:
         except ValueError:
             exit(58)  # Error
 
+    def instr_int2chars(self) -> None:
+        value = self._get_values_from_stack(1)
+        if type(value) is not int:
+            exit(53)  # Error
+        try:
+            value = chr(value)
+        except ValueError:
+            exit(58)  # Error
+        self._replace_stack_value(value, 1)
+
+    def instr_int2float(self, var: str, symb: str) -> None:
+        value = self._get_value(symb)
+        if type(value) is not int:
+            exit(53)  # Error
+        try:
+            self._set_value(var, float(value))
+        except ValueError:
+            exit(58)  # Error
+
+    def instr_float2int(self, var: str, symb: str) -> None:
+        value = self._get_value(symb)
+
+        if type(value) is not float:
+            exit(53)  # Error
+        try:
+            self._set_value(var, int(value))
+        except ValueError:
+            exit(58)  # Error
+
     def instr_stri2int(self, var: str, symb1: str, symb2: str) -> None:
         value, index = self._get_some_values(symb1, symb2)
         if not isinstance(value, str) or type(index) is not int:
@@ -236,6 +355,15 @@ class Function:
         elif index >= len(value):
             exit(58)  # Error
         self._set_value(var, str(ord(value[index])))
+
+    def instr_stri2ints(self) -> None:
+        index, value = self._get_values_from_stack(2)
+        if not isinstance(value, str) or type(index) is not int:
+            exit(53)  # Error
+        elif index >= len(value):
+            exit(58)  # Error
+        value = str(ord(value[index]))
+        self._replace_stack_value(value, 2)
 
     def instr_read(self, var: str, type_: str) -> None:
         value = input() if self.stdin is None else self.stdin
@@ -250,6 +378,11 @@ class Function:
                 value = int(value)
             except ValueError:
                 value = None
+        elif type_ == 'float':
+            try:
+                value = float.fromhex(value)
+            except ValueError:
+                value = None
         self._set_value(var, value)
 
     def instr_write(self, symb: str) -> None:
@@ -258,6 +391,8 @@ class Function:
             print('true' if value else 'false', end='')
         elif value is None:
             print('', end='')
+        elif type(value) is float:
+            print(float.hex(value), end='')
         else:
             print(value, end='')
 
@@ -315,12 +450,30 @@ class Function:
         if value1 == value2:
             self.instr_jump(label)
 
+    def instr_jumpifeqs(self, label: str) -> None:
+        value2, value1 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) and value1 is not None and value2 is not None:
+            exit(53)  # Error
+        if value1 == value2:
+            self.instr_jump(label)
+        self.stack.pop()
+        self.stack.pop()
+
     def instr_jumpifneq(self, label: str, symb1: str, symb2: str) -> None:
         value1, value2 = self._get_some_values(symb1, symb2)
         if type(value1) != type(value2) and value1 is not None and value2 is not None:
             exit(53)  # Error
         if value1 != value2:
             self.instr_jump(label)
+
+    def instr_jumpifneqs(self, label: str) -> None:
+        value2, value1 = self._get_values_from_stack(2)
+        if type(value1) != type(value2) and value1 is not None and value2 is not None:
+            exit(53)  # Error
+        if value1 != value2:
+            self.instr_jump(label)
+        self.stack.pop()
+        self.stack.pop()
 
     def instr_exit(self, symb: str) -> None:
         value = self._get_value(symb)
@@ -340,9 +493,9 @@ class Function:
     def _get_value(self, symb: str) -> any:
         value = symb.split('@', 1)
         if value[0] in {'GF', 'LF', 'TF'}:
-            if self.frames[value[0]] is None:
+            if value[0] not in self.frames or self.frames[value[0]] is None:
                 exit(55)  # Error
-            if value[1] not in self.frames[value[0]]:
+            elif value[1] not in self.frames[value[0]]:
                 exit(54)  # Error
             return self.frames[value[0]][value[1]]
         elif value[0] == 'int':
@@ -351,14 +504,27 @@ class Function:
             return value[1].lower() == 'true'
         elif value[0] == 'nil':
             return None
+        elif value[0] == 'float':
+            return float(value[1])
         return value[1]
 
     def _get_some_values(self, *args: str) -> list:
         return [self._get_value(arg) for arg in args]
 
+    def _get_values_from_stack(self, count: int) -> any:
+        values = [self.stack[-num-1] for num in range(count)]
+        return values if len(values) != 1 else values[0]
+
+    def _replace_stack_value(self, value: any, count: int) -> None:
+        for _ in range(count):
+            self.stack.pop()
+        self.stack.append(value)
+
     def _set_value(self, var: str, value: any) -> None:
         var_frame, var_name = self._var_split(var)
-        if var_name not in self.frames[var_frame]:
+        if var_frame not in self.frames:
+            exit(55)
+        elif var_name not in self.frames[var_frame]:
             exit(54)  # Error
         self.frames[var_frame][var_name] = value
 
@@ -367,7 +533,7 @@ class Function:
         value1, value2 = self._get_some_values(symb1, symb2)
 
         if op in {'+', '-', '*'}:
-            if type(value1) is not int or type(value2) is not int:
+            if type(value1) != type(value2) or type(value1) not in (int, float) or type(value2) not in (int, float):
                 exit(53)  # Error
             return eval(f'value1 {op} value2')
         if op in {'<', '>'}:
@@ -378,12 +544,18 @@ class Function:
             if type(value1) != type(value2) and value1 is not None and value2 is not None:
                 exit(53)  # Error
             return value1 == value2
-        elif op == '/':
-            if type(value1) is not int or type(value2) is not int:
+        elif op == '//':
+            if type(value1) != type(value2) or type(value1) not in (int, float) or type(value2) not in (int, float):
                 exit(53)  # Error
             elif value2 == 0:
                 exit(57)  # Error
-            return int(value1) // int(value2)
+            return value1 // value2
+        elif op == '/':
+            if type(value1) != type(value2) or type(value1) not in (int, float) or type(value2) not in (int, float):
+                exit(53)  # Error
+            elif value2 == 0:
+                exit(57)  # Error
+            return value1 / value2
         elif op == '.':
             if not isinstance(value1, str) or not isinstance(value2, str):
                 exit(53)  # Error
@@ -446,7 +618,7 @@ class Interpreter:
         for arg in instruction.args:
             if arg.type_.lower() in {'var', 'label', 'type'}:
                 result.append(arg.text)
-            elif arg.type_.lower() in {'int', 'bool', 'string'}:
+            elif arg.type_.lower() in {'int', 'bool', 'string', 'float'}:
                 result.append(f'{arg.type_}@{arg.text}')
             elif arg.type_.lower() == 'nil':
                 result.append('nil@nil')
@@ -464,9 +636,10 @@ class Interpreter:
         for num, arg in enumerate(func_args):
             if arg == 'var' and args[num].split('@')[0] not in {'GF', 'LF', 'TF'}:
                 exit(52)  # Error
-            elif 'symb' in arg and args[num].split('@')[0] not in {'GF', 'LF', 'TF', 'int', 'bool', 'string', 'nil'}:
+            elif 'symb' in arg and args[num].split('@')[0] not in {'GF', 'LF', 'TF', 'int',
+                                                                   'bool', 'string', 'nil', 'float'}:
                 exit(52)  # Error
-            elif arg == 'type_' and args[num] not in {'int', 'string', 'bool', 'label', 'nil'}:
+            elif arg == 'type_' and args[num] not in {'int', 'string', 'bool', 'label', 'nil', 'float'}:
                 exit(32)  # Error
 
 
